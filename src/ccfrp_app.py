52af0b10433ab16ef225344033fd9f6d13dc9e6e
Connor <cddibble@gmail.com>
2022-08-14 00:53:49 -0700

    more updates so we have a basic ability to query the tables and display them

--- src/ccfrp_app.py
+++ src/ccfrp_app.py
@@ -1,16 +1,51 @@
 # import pandas as pd
-from flask import Flask
+from flask import Flask, render_template, request
 from datetime import datetime, timedelta
 # from tinyflux import TinyFlux
-import wrangling
+import sys
+import os
+sys.path.insert(0, os.path.dirname(__file__))
+from api import Api
+# from . import app
 
-app = Flask(__name__)
+
+def create_app():
+    app = Flask(__name__, template_folder='templates')
+    from . import auth
+    app.register_blueprint(auth.bp)
+    return app
+
+app = create_app()
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template('base.html')
+
 
 @app.route("/home")
 def home_page():
     return f"hello. it is {datetime.utcnow()}."
 
+
+@app.route("/fish/length", methods = ['GET', 'POST'])
+def fish_length():
+    api = Api()
+    if request.method == 'GET':
+        return render_template(
+            'select_fish.html',
+            fishes = (api.species.Common_Name.unique(), api.species.Common_Name.unique()[0]),
+            dates = (sorted(api.length.Date.unique()), api.length.Date.min())
+            )
+    elif request.method == 'POST':
+        df = api.get_df(
+            'length',
+            common_name=request.form.get('fish'),
+            start_date =request.form.get('start_date'),
+            )
+        print(request.form)
+        return render_template(
+            'select_fish.html',
+            fishes = (api.species.Common_Name.unique(), request.form.get('fish')),
+            table = df.to_html(),
+            dates = (sorted(api.length.Date.unique()), request.form.get('start_date'))
+            )
