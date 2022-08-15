a3d118075c407469b92b128d4b4a6b0a4b2ec729
Connor <cddibble@gmail.com>
2022-08-15 00:03:15 -0700

    uses form for time and fish selection

--- src/ccfrp_app.py
+++ src/ccfrp_app.py
@@ -1,21 +1,33 @@
 # import pandas as pd
 from flask import Flask, render_template, request
+# from flask_bootstrap import Bootstrap
+# from flask_datepicker import datepicker
+
 from datetime import datetime, timedelta
 # from tinyflux import TinyFlux
 import sys
 import os
 sys.path.insert(0, os.path.dirname(__file__))
 from api import Api
-# from . import app
+from wtforms.fields import DateField, SubmitField, SelectField
+from wtforms.validators import InputRequired
+from flask_wtf import FlaskForm
 
 
 def create_app():
     app = Flask(__name__, template_folder='templates')
+    app.config['SECRET_KEY'] = 'poopypants'
     from . import auth
     app.register_blueprint(auth.bp)
     return app
 
 app = create_app()
+
+class TimeForm(FlaskForm):
+    start_date = DateField('start_date')
+    end_date = DateField('end_date')
+    submit = SubmitField('Submit')
+    
 
 @app.route("/")
 def index():
@@ -30,22 +42,29 @@ def home_page():
 @app.route("/fish/length", methods = ['GET', 'POST'])
 def fish_length():
     api = Api()
-    if request.method == 'GET':
+    all_species=api.species.Common_Name.unique()
+    TimeForm.fish_name = SelectField(u'Field name', choices = all_species, validators = [InputRequired()])
+    form = TimeForm()
+    if not form.is_submitted():
+        default_start = api.length.Date.min().date().isoformat()
+        default_end = (api.length.Date.max() + timedelta(days=1)).date().isoformat()
         return render_template(
             'select_fish.html',
-            fishes = (api.species.Common_Name.unique(), api.species.Common_Name.unique()[0]),
-            dates = (sorted(api.length.Date.unique()), api.length.Date.min())
+            form = form,
+            default_start = default_start,
+            default_end = default_end
             )
-    elif request.method == 'POST':
+    else:
         df = api.get_df(
             'length',
-            common_name=request.form.get('fish'),
-            start_date =request.form.get('start_date'),
+            common_name=form.data.get('fish_name'),
+            start_time=form.data.get('start_date'),
+            end_time=form.data.get('end_date'),
             )
-        print(request.form)
         return render_template(
             'select_fish.html',
-            fishes = (api.species.Common_Name.unique(), request.form.get('fish')),
-            table = df.to_html(),
-            dates = (sorted(api.length.Date.unique()), request.form.get('start_date'))
+            form=form,
+            table = df.sort_values('Date').to_html(),
+            default_start = form.data.get('start_date'),
+            default_end = form.data.get('end_date'),
             )
